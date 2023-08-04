29a18d8b84700d8962b7291f287217afe90d6667
Qu3zada22 <139565119+Qu3zada22@users.noreply.github.com>
2023-08-04 09:31:56 -0600

    Cambio 1 de escritor
    Generar código para escribir en el archivo

--- Escritor.java
+++ Escritor.java
@@ -9,44 +9,36 @@ import java.io.FileReader;
 // un poco de ayuda de chat gpt
 class Escritor{
     private List<String> palindromas;
+    
+    // Variable para mantener el archivo de salida (Agrgada, no en UML)
+    private File archivoSalida;
+
     public void crear(String csv) {
-        try {
-            // Crear un objeto File para el archivo de salida
-            File archivoSalida = new File("salida.txt"); // Puedes cambiar el nombre y ubicación si es necesario
+        // Crear un objeto File para el archivo de salida
+        archivoSalida = new File("salida.csv");
 
-            // Crear FileWriter y BufferedWriter para escribir en el archivo
-            FileWriter escritorArchivo = new FileWriter(archivoSalida);
-            BufferedWriter bufferEscritura = new BufferedWriter(escritorArchivo);
-
-            // Escribir el contenido del CSV en el archivo
-            bufferEscritura.write(csv);
-
-            // Cerrar BufferedWriter y FileWriter para liberar recursos
-            bufferEscritura.close();
-            escritorArchivo.close();
-
-            System.out.println("Contenido del CSV ha sido escrito en el archivo.");
-        } catch (IOException e) {
-            System.err.println("Error al escribir el archivo: " + e.getMessage());
-        }
+        System.out.println("Archivo CSV ha sido creado.");
     }
 
     public void escribir(List<String> palindromas) {
         try {
-            // Crear un objeto File para el archivo de salida
-            File archivoSalida = new File("palindromas.txt"); // Puedes cambiar el nombre y ubicación si es necesario
+            // Verificar el archivo
+            if (archivoSalida == null) {
+                System.err.println("Error: El archivo de salida no ha sido creado.");
+                return;
+            }
 
             // Crear FileWriter y BufferedWriter para escribir en el archivo
             FileWriter escritorArchivo = new FileWriter(archivoSalida);
             BufferedWriter bufferEscritura = new BufferedWriter(escritorArchivo);
 
-            // Escribir las palabras palindrómicas en el archivo
+            // Escribir las palabras en el archivo
             for (String palabra : palindromas) {
                 bufferEscritura.write(palabra);
-                bufferEscritura.newLine(); // Agregar una nueva línea después de cada palabra
+                bufferEscritura.newLine();
             }
 
-            // Cerrar BufferedWriter y FileWriter para liberar recursos
+            // Cerrar BufferedWriter y FileWriter
             bufferEscritura.close();
             escritorArchivo.close();
 
@@ -55,6 +47,7 @@ class Escritor{
             System.err.println("Error al escribir el archivo: " + e.getMessage());
         }
     }
+    
     public void mostrar(String csv) {
         try {
             // Crear un objeto File para el archivo de entrada (CSV)
